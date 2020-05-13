1965caca64c6f14ff8973b84c7d15d7998743a7c
Encacap <encacap@gmail.com>
2020-05-13 12:15:14 +0700

    Improve basic features

--- object-oriented-programing/inheritance/inheritance.cpp
+++ object-oriented-programing/inheritance/inheritance.cpp
@@ -2,8 +2,14 @@
 
 BieuThucSoHoc::BieuThucSoHoc()
 {
-    srand((int)time(0));
+    this->point = 0;
+    this->score = 0;
+}
+
+void BieuThucSoHoc::createQuestion()
+{
     this->level = this->randomNumber(1, 5);
+    this->point = level;
     switch (level)
     {
     case 1:
@@ -43,21 +49,20 @@ int BieuThucSoHoc::selectOperator(char allowOperator[])
 
 int BieuThucSoHoc::printQuestion()
 {
+    this->createQuestion();
+    this->point = this->level;
     int level = this->level;
     int length = strlen(this->pheptoan);
     int operands[7] = {0};
+    int operators[7] = {0};
     int result = 0;
+    cout << LINE << endl;
     cout << "Level: " << level << endl;
+    cout << "Current score: " << this->score << endl;
+    cout << ">> ";
     for (int i = 0; i <= length * 2; i++)
     {
-        if (i == 0)
-        {
-            int num = this->randomNumber(1, max);
-            result = num;
-            operands[0] = 0;
-            cout << num;
-        }
-        else if (i % 2 == 0)
+        if (i % 2 == 0)
         {
             int num = this->randomNumber(1, max);
             operands[i] = num;
@@ -65,40 +70,72 @@ int BieuThucSoHoc::printQuestion()
         }
         else
         {
-            int operatorChar = this->selectOperator(this->pheptoan);
-            switch (operatorChar)
-            {
-            case 0:
-                result += operands[i - 1];
-                cout << " + ";
-                break;
-            case 1:
-                result -= operands[i - 1];
-                cout << " - ";
-                break;
-            case 2:
-                result *= operands[i - 1];
-                cout << " * ";
-                break;
-            default:
-                break;
-            }
-            // a + b + c + d
-            // 1 2 3 4 5 6 7
+            int o = this->selectOperator(this->pheptoan);
+            operators[i] = o + 1;
+            cout << " " << this->pheptoan[o] << " ";
         }
     }
 
-    cout << endl
-         << result;
+    cout << " = ";
 
-    return 0;
+    for (int i = 0; i < 7; i++)
+        if (operators[i] == 3)
+        {
+            if (operands[i - 1] == 0)
+            {
+                result *= operands[i + 1];
+                operands[i + 1] = 0;
+            }
+            else
+            {
+                result += operands[i - 1] * operands[i + 1];
+                operands[i - 1] = operands[i + 1] = 0;
+            }
+            operators[i] = 0;
+        }
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (operators[i] == 1)
+            result += operands[i - 1] + operands[i + 1];
+        else
+            result += operands[i - 1] - operands[i + 1];
+        operands[i - 1] = operands[i + 1] = 0;
+    }
+
+    return result;
+}
+
+void BieuThucSoHoc::updateScore()
+{
+    this->score += this->point;
 }
 
 int main()
 {
+    srand((int)time(0));
+    int questionsPerTurn = 5; // Number of questions per turn
+    int answer;
+    int question = 0;
     BieuThucSoHoc expresstions;
     BieuThucCong plusExpresstions;
+    cout << "Each round will have " << questionsPerTurn << " question." << endl;
+    cout << "The number of points received will correspond to the level of the question" << endl;
+    for (int i = 0; i < questionsPerTurn; i++)
+    {
+        question = expresstions.printQuestion();
+        cin >> answer;
+        if (answer == question)
+        {
+            cout << "Congratulation!";
+            expresstions.updateScore();
+        }
+        else
+        {
+            cout << "Incorrect";
+        }
+        cout << endl;
+    }
 
-    expresstions.printQuestion();
     return 0;
 }
--- object-oriented-programing/inheritance/inheritance.h
+++ object-oriented-programing/inheritance/inheritance.h
@@ -4,18 +4,22 @@
 #include <cstring>
 #include <random>
 
+#define LINE "=================================="
+
 using namespace std;
 
 class BieuThucSoHoc
 {
 public:
-    int a, b, level, max = 10;
+    int level, max, point, score = 0;
     char *pheptoan;
     BieuThucSoHoc();
     int randomNumber(int min, int max);
     int printQuestion();
     int selectOperand(int maximum);
     int selectOperator(char allowOperator[]);
+    void createQuestion();
+    void updateScore();
 };
 
 class BieuThucCong : public BieuThucSoHoc
