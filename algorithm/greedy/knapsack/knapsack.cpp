99de6f88d027f7f40c2924d29796e93e96a25be4
Encacap <encacap@gmail.com>
2020-05-23 06:14:32 +0700

    Fraction Knapsack

---
+++ algorithm/greedy/knapsack/knapsack.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// Structure for an item which stores weight and corresponding
+// value of Item
+struct Item
+{
+    int value, weight;
+
+    // Constructor
+    Item(int value, int weight) : value(value), weight(weight) {}
+};
+
+// Comparison function to sort Item according to val/weight ratio
+bool cmp(struct Item a, struct Item b)
+{
+    double r1 = (double)a.value / a.weight;
+    double r2 = (double)b.value / b.weight;
+    return r1 > r2;
+}
+
+double fractionalKnapsack(int W, struct Item arr[], int n)
+{
+    sort(arr, arr + n, cmp);
+
+    int curWeight = 0;
+    double finalValue = 0.0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (curWeight + arr[i].weight <= W)
+        {
+            curWeight += arr[i].weight;
+            finalValue += arr[i].value;
+        }
+        else
+        {
+            int remain = W - curWeight;
+            finalValue += arr[i].value * ((double)remain / arr[i].weight);
+            break;
+        }
+    }
+
+    return finalValue;
+}
+
+int main()
+{
+    int W = 60;
+    Item arr[] = {{30, 18}, {100, 40}, {50, 25}, {34, 15}};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Maximum value we can obtain = " << fractionalKnapsack(W, arr, n);
+    return 0;
+}
